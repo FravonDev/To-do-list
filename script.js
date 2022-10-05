518fb80401d8a4211f1a7f6524610cea8c143ea0
flavinpv <flvsadr@gmail.com>
2022-10-05 09:54:51 -0300

    📚função para validar input

--- script.js
+++ script.js
@@ -13,6 +13,19 @@ class Card{
         console.log(JSON.stringify(this.tasks))
 
     }
+    // função para validar se a string está vazia, tem espaços em branco.
+    validateDescription(description){
+            if(description.trim() != ""){
+                //TODO:
+                console.log(description)
+                //return is valid
+                return true;
+
+            }
+        
+        return new Error('Campo vazio')
+
+    }
     deleteTask(description){
         this.tasks = this.tasks.filter(function(element) { return element.description !== description; });
     }
@@ -20,17 +33,17 @@ class Card{
         console.log(this.tasks.find(task => task.description === description))
     }
     changeTask(){
-        //todo
+        //TODO:
     }
-
     // transformar em elemento HTML
     updateTask(){
+        //TODO:
     }
 }
 
 class Task{
     constructor(status, description ){
-        this.status = status
+        this.status = false
         this.description = description
         // make a unique id
     }
@@ -52,12 +65,17 @@ shortCard.createNewTask('checked','se organizar');
 const form = document.querySelectorAll('.addTask').forEach(element => {
     element.addEventListener('submit', e => {
         e.preventDefault() // remover o redirect padrão do form.
+        //TODO: get the currentCard
 
-        // mostrar o valor do input
-        console.log(element.querySelector('input').value)
+        // pegar e guardar o valor do input
+        let inputValue = element.querySelector('input').value
+        // verificar se o valor é valido.
+        shortCard.validateDescription(inputValue)
         // TODO:
 
-        console.log(e)
-    console.log('Deu certo')
+
+        // crie uma nova task com esta description no card
+        shortCard.createNewTask()
+        console.log('pegamos dados do form')
     })
 });
