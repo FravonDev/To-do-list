e44ce5e2fa7a0736f36e832c23334ee6cd6411cf
flavinpv <flvsadr@gmail.com>
2022-10-05 20:50:02 -0300

    📚  salvando dados noLocalStorage, novos cards

--- script.js
+++ script.js
@@ -8,14 +8,15 @@ class Card{
 
     createNewTask(description){
         this.tasks.push(new Task(description));
-        console.log(JSON.stringify(this.tasks))
+                
+        //Guardando os dados do card Atual, na localStorage como JSON
+        localStorage.setItem(this.name,JSON.stringify(this.tasks))
 
+        console.log(JSON.stringify(this.tasks))
     }
     // função para validar se a string está vazia, tem espaços em branco.
     validateDescription(description){
             if(description.trim() != ""){
-                //TODO:
-                console.log(description)
                 //return is valid
                 return true;
             }
@@ -29,12 +30,14 @@ class Card{
     queryTask(description){
         console.log(this.tasks.find(task => task.description === description))
     }
-    changeTask(){
+    updateTask(){
         //TODO:
     }
     // transformar em elemento HTML
-    updateTask(){
+    refreshCards(){
         //TODO:
+        this.tasks = JSON.parse(localStorage.getItem(this.name))
+
     }
 }
 
@@ -42,41 +45,53 @@ class Task{
     constructor(description ){
         this.status = false
         this.description = description
-        // make a unique id
     }
-    
 }
-
-//criar card 
+//criar os 3 cards 
 const shortCard = new Card('shortCard');
-
-shortCard.createNewTask('checked','verify');
-shortCard.createNewTask('unchecked','soner blaner');
-shortCard.createNewTask('checked','se organizar');
+const mediumCard = new Card('mediumCard')
+const longCard = new Card('longCard')
 
 
-console.log(shortCard)
-
+shortCard.refreshCards()
 
 //Quando o form for enviado, Escutar pelo evento submit em cada form
-const form = document.querySelectorAll('.addTask').forEach(element => {
-    element.addEventListener('submit', e => {
+document.querySelectorAll('.card').forEach(card => {
+    card.querySelector('.addTask').addEventListener('submit', e => {
         e.preventDefault() // remover o redirect padrão do form.
-        
-        console.log('pegamos dados do form')
-
         //TODO: pegar o card atual, que está sendo utilizado.
-
         // pegar e guardar o valor do input
-        let inputValue = element.querySelector('input').value
+        let inputValue = card.querySelector('input').value
 
-        // verificar se o valor é valido.
-        let validated = shortCard.validateDescription(inputValue)
-        // TODO: inser
-        if (validated){
-            // crie uma nova task com esta description no card
-            shortCard.createNewTask(inputValue)
-        }
+        validationsCardObject(inputValue, card)
 
     })
 });
+
+
+
+function validationsCardObject(inputValue, currentCard){
+    switch(currentCard.id){
+        case shortCard.name:
+            currentCard = shortCard
+            break;
+        case mediumCard.name:
+            currentCard = mediumCard
+            break;
+        case longCard.name:
+            currentCard = longCard
+            break;
+    }
+
+
+    console.log(currentCard)
+
+    // verificar se o valor é valido.
+    let validated = currentCard.validateDescription(inputValue)
+
+    if (validated){
+        // crie uma nova task com esta description no card
+        currentCard.createNewTask(inputValue)
+        currentCard.refreshCards()
+    }
+}
