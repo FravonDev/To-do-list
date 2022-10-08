2055c7eee31000c14376e8ae95464ebfc315795f
flavinpv <flvsadr@gmail.com>
2022-10-08 18:43:15 -0300

    📚 Funcionando com  suas Classes e Métodos

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ class Task {
     this.description = description;
   }
 }
-
 //criar a classe card, que é onde ficam nossas tarefas
 class Card {
   name;
@@ -14,35 +13,26 @@ class Card {
   constructor(name) {
     this.name = name;
   }
-
   // 1 Método para imprimir
   printTask(status, description) {
-    // validar se a descrição é valida.
     if (this.validateDescription(description) == false) {
-      // se for inválida, interrompa a execução do método
       return false;
     }
 
-    // criar e adicionar valores no checkbox.
     let checkbox = document.createElement("input");
     checkbox.setAttribute("type", "checkbox");
     checkbox.checked = status;
 
-    // criar e adicionar valores na label
     let label = document.createElement("label");
     label.setAttribute("contentEditable", "true");
     label.innerText = description;
 
-    //criar um botão para remover uma tarefa.
     let removeButton = document.createElement("button");
     removeButton.setAttribute("class", "removeButton");
 
-    //criar uma div para agrupar o checkbox e na label.
     let groupTaskElements = document.createElement("div");
-    //adicionar classe
     groupTaskElements.setAttribute("class", "taskGroup");
 
-    //inserir os elementos na div
     groupTaskElements.append(checkbox);
     groupTaskElements.append(label);
     groupTaskElements.append(removeButton);
@@ -52,50 +42,28 @@ class Card {
     cardElement = cardElement.querySelector(".tasks");
     cardElement.append(groupTaskElements);
 
-    //TODO:
-    /* depois que colocar os elementos na tela, atualize os dados da localStorage para eliminar
-        dados em brancos criados pela edição da tarefa
-        para espaço em branco ou tarefa vazia */
   }
   // 2 Método de carregar os dados.
   loadElements() {
-    // pegue as tasks que etão guardadas no localStorage
     let tasks = localStorage.getItem(`${this.name}`);
-    /* se não tiver nenhuma tarefa,
-         nao tem o que ser carregado, então pare a execução. */
     if (!tasks) {
       return false;
     }
-    // guarde essas tasks no seu respectivo card.
     tasks = JSON.parse(tasks);
     this.tasks = tasks;
 
-    //adicione cada elemento na tela.
     this.tasks.forEach((task) => this.printTask(task.status, task.description));
   }
   //3 Método para Guardar novos dados
   insertNewTask() {
-    //pegar o card atual.
     let currentCard = document.querySelector(`#${this.name}`);
-    //pegar o form nele
     let form = currentCard.querySelector(".addTask");
-    /*quando o form for enviado,
-         Pegamos o valor vindo do input text*/
     form.onsubmit = (e) => {
+      
       e.preventDefault();
       let inputValue = currentCard.querySelector(".addTaskInput").value;
-
-      console.log(inputValue);
-      console.log(this.tasks);
-
-      //adicionar no array do card.
       this.tasks.push(new Task(false, inputValue));
-
-      //mostrar a nova tarefa na tela
       this.printTask(false, inputValue);
-      console.log(this.tasks[this.tasks.length - 1]);
-
-      // salvar os dados no localStorage
       localStorage.setItem(`${this.name}`, JSON.stringify(this.tasks));
     };
   }
@@ -103,9 +71,7 @@ class Card {
 //Método para atualizar
 updateTask(index, element) {
   let tasks = JSON.parse(localStorage.getItem(`${this.name}`));
-  
-  //FIXME:
-  // se a task 1 estiver vazia.
+
   if (tasks[0].description == undefined){
     tasks.splice(0,1)
   }
@@ -114,19 +80,16 @@ updateTask(index, element) {
   tasks[index].description = element.querySelector('label').innerText
   this.tasks = tasks;
 
-  // atualizar localstorage
   localStorage.setItem(`${this.name}`, JSON.stringify(tasks));
   localStorage.setItem(JSON.stringify(`${this.name}`), tasks)
 
 }
+
   // Método para verificar alterações nos elementos.
   listenForChanges() {
-  //TODO:
-    //pegar o card (elemento html) referente ao objeto card (objeto).
     let currentCard = document.querySelector(`#${this.name}`);
     console.log(`e isso ai`)
 
-    //escutar por alteração no checkbox.
     currentCard.querySelectorAll('input[type="checkbox"]').forEach((checkbox) => {
       checkbox.onclick = (event) => {
 
@@ -136,7 +99,6 @@ updateTask(index, element) {
       };
     });
 
-     //escutar por alteração no checkbox.
      currentCard.querySelectorAll('label').forEach((label) => {
       label.onblur = (event) => {
 
@@ -150,7 +112,6 @@ updateTask(index, element) {
 
     currentCard = document.querySelector(`#${this.name}`);
 
-    // Remover elementos
     currentCard.querySelectorAll(".removeButton").forEach((element) => {
       element.onclick = (event) => {
         let array = Array.from(element.parentNode.parentNode.children);
@@ -160,36 +121,21 @@ updateTask(index, element) {
       };
     });
   }
-
-  //Método para remover tarefas
   removeTask(index, element) {
-    console.log(index, element, this);
-
-    //pegar tasks armazenadas e converte-las
     let tasks = JSON.parse(localStorage.getItem(`${this.name}`));
-
     tasks.splice(index, 1);
-
-    // atualizar localstorage
     localStorage.setItem(`${this.name}`, JSON.stringify(tasks));
-    //atualizar no array do objeto
     this.tasks = tasks;
 
-    //remover da tela
-    console.log(element.parentNode.remove());
-
-    //após remover escutar por novos eventos
-    this.listenForChanges;
   }
 
   //2.1 validar descrição
   validateDescription(description) {
     if (typeof description != "string" || description.trim() == "") {
-      //se for invalida, não faça nada.
       return false;
     }
   }
-  // função para atualizar o localstorage
+  // 4 método para atualizar o localstorage
   refreshStorage() {
     localStorage.setItem(
       localStorage.setItem(this.name, JSON.stringify(this.tasks))
@@ -197,16 +143,13 @@ updateTask(index, element) {
   }
 }
 
-//criar os 3 cards
 const shortCard = new Card("shortCard");
 const mediumCard = new Card("mediumCard");
 const longCard = new Card("longCard");
 
-//habilitar todos métodos dos objetos
 [shortCard, mediumCard, longCard].forEach(card => {
   card.loadElements();
   card.insertNewTask();
   card.listenForChanges();
-
 })
 
